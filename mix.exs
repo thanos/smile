81fdd3867fcca48d58bff2fa2248e2e09b956d66
thanos vassilakis <thanos@syntazo.com>
2018-11-04 01:26:17 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Smile.MixProject do
     [
       app: :smile,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
